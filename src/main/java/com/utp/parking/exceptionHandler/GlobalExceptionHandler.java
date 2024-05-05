b2caa4a671feaa120522ff44d7c0b4f853d6bd98
sebastian-reyes <sebastianreyes2105@gmail.com>
2024-05-04 23:22:13 -0500

    Agregando manejador de Excepciones

--- src/main/java/com/utp/parking/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/utp/parking/exceptionHandler/GlobalExceptionHandler.java
@@ -1,2 +1,20 @@
-package com.utp.parking.exceptionHandler;public class GlobalExceptionHandler {
+package com.utp.parking.exceptionHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handlerArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<String>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handlerRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_GATEWAY);
+    }
 }
